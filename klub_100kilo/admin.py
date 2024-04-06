aa3cfe72bd8e1b9ead71971b833c0db7e32ebd64
Dawid Mularczyk <muldaw@student.agh.edu.pl>
2024-04-06 16:23:05 +0200

    Initial commit

---
+++ klub_100kilo/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from .models import AuthGroup, AuthUser, Diet, Exercises, Gyms, Measurements, Reservations, Users
+
+
+# Custom admin display for AuthGroup
+@admin.register(AuthGroup)
+class AuthGroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+# Custom admin display for AuthUser
+@admin.register(AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "date_joined")
+    search_fields = ("username", "email")
+    list_filter = ("is_active", "is_staff")
+
+
+# Custom admin display for Diet
+@admin.register(Diet)
+class DietAdmin(admin.ModelAdmin):
+    list_display = ("date", "user", "breakfest", "dinner", "other")
+
+
+# Custom admin display for Exercises
+@admin.register(Exercises)
+class ExercisesAdmin(admin.ModelAdmin):
+    list_display = ("exercise_id", "description", "muscle_group", "difficulty", "category")
+
+
+# Custom admin display for Gyms
+@admin.register(Gyms)
+class GymsAdmin(admin.ModelAdmin):
+    list_display = ("gym_id", "name", "phone_number", "address")
+
+
+# Custom admin display for Measurements
+@admin.register(Measurements)
+class MeasurementsAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "user",
+        "weight",
+        "biceps_size",
+        "bust_size",
+        "waist_size",
+        "dick_size",
+        "thighs_size",
+        "height",
+    )
+
+
+# Custom admin display for Reservations
+@admin.register(Reservations)
+class ReservationsAdmin(admin.ModelAdmin):
+    list_display = ("reservation_id", "user", "type", "status", "gym", "trainer_id", "date")
+    list_filter = ("type", "status")
+
+
+# Custom admin display for Users
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "role", "first_name", "last_name", "mail", "phone_number")
+    search_fields = ("first_name", "last_name", "mail")
+    list_filter = ("role",)
---
+++ klub_100kilo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Klub100KiloConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "klub_100kilo"
