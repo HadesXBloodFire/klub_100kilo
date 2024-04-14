25199e98c81290fd90f01b34b4a21c7690729c4d
Maciej Wilewski <m.m.wilewski@gmail.com>
2024-04-14 20:45:16 +0200

    Hero page, main page, navbar,title,background etc, mainpage is blocked because no loginyet

--- klub_100kilo/views.py
+++ klub_100kilo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'main.html')
+
+def hero_page(request):
+    if request.user.is_authenticated:
+        return redirect('main_page')
+    return render(request, 'hero.html')
+
+@login_required
+def main_page(request):
+    return render(request, 'main.html')
