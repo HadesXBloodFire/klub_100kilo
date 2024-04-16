647a7f0961cd0088458da419a486a1fd43deef09
Maciej Wilewski <m.m.wilewski@gmail.com>
2024-04-16 16:03:03 +0200

    Login error handling

--- klub_100kilo/forms.py
+++ klub_100kilo/forms.py
@@ -23,8 +23,6 @@ class RegisterForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise ValidationError("Email already exists")
         return email
 
     def save(self, commit=True):
